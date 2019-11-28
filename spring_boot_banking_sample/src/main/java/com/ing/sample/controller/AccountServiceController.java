02f7094ea518195596672eb261217ae4587bca2f
GobinathVengatasan <gobi.gobi.v@gmail.com>
2019-11-28 17:02:10 +0530

    Accounts Model Added and Get Services Added

--- spring_boot_banking_sample/src/main/java/com/ing/sample/controller/AccountServiceController.java
+++ spring_boot_banking_sample/src/main/java/com/ing/sample/controller/AccountServiceController.java
@@ -1,16 +1,47 @@
 package com.ing.sample.controller;
 
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ing.sample.model.Account;
+import com.ing.sample.model.AccountStatusEnum;
+import com.ing.sample.model.AccountTypeEnum;
+import com.ing.sample.model.UserAccounts;
 
 @RestController
 @RequestMapping("/bank")
 public class AccountServiceController {
 
 	@GetMapping("/account")
-	public String welcome() {		
-		return "Gobinath V";
+	public UserAccounts getAllAccounts() {		
+		return getMockUserAccounts();
+	}
+	
+	@GetMapping("/account/{accountId}")
+	public Account getAccount(@PathVariable String accountId) {		
+		return getMockUserAccounts().getAllAccounts().stream().filter(it-> it.getId()==Integer.parseInt(accountId)).findFirst().orElse(null);
+	}
+		
+	private UserAccounts getMockUserAccounts() {
+		UserAccounts userAccounts = new UserAccounts();
+		userAccounts.getAllAccounts().add(getMockAccount());
+		return userAccounts;
+	}
+
+	private Account getMockAccount() {
+		Account account = new Account();
+		account.setAccountNumber("123Saving456");
+		account.setAccountStatus(AccountStatusEnum.ACTIVE);
+		account.setAccountType(AccountTypeEnum.SAVING);
+		account.setBalance(15.4f);
+		account.setBankName("International Nertherland Group");
+		account.setHolderName("Gobinath Vengatasan");
+		account.setId(1);
+		return account;		
 	}
 
 }
